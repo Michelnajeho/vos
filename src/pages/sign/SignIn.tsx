3d21853a42224785427f1f1d99c1ce61c22dcd92
michelanjeho <michelanjeho@gmail.com>
2024-07-09 05:49:00 +0900

    Sign in useState()

--- src/pages/sign/SignIn.tsx
+++ src/pages/sign/SignIn.tsx
@@ -1,6 +1,12 @@
+import { useLocation } from 'react-router-dom';
 import Container from '../../components/layout/Container';
 
 const Login = () => {
+
+  const location = useLocation();
+  const userInfo = { ...location.state };
+  console.log(userInfo);
+
   return (
     <Container>
       로그인 페이지
--- src/pages/sign/SignUp.tsx
+++ src/pages/sign/SignUp.tsx
@@ -1,9 +1,50 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import Container from '../../components/layout/Container';
 
 const SignUp = () => {
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  const idInputOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value);
+  }
+  
+  const passwordInputOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
+  const singUp = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    // validation
+    // 
+    console.log('id:', id);
+    console.log('password:', password);
+
+    navigate("/sign-in", {
+      replace : false,
+      state: {
+        data:"asd"
+      },
+    });
+
+  }
+
   return (
     <Container>
-      회원가입 페이지
+      <div>
+        <h1>회원가입 페이지</h1>
+        <div>
+          <input type="text" id="id" placeholder='아이디' onChange={idInputOnChange} />
+          <input type="password" id="password" placeholder='비밀번호' onChange={passwordInputOnChange} />
+          <button onClick={singUp}>회원가입</button>
+        </div>
+      </div>
     </Container>
   )
 }
